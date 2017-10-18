03ae112cb56f96e9b4b281b29bfb989c838fb8a1
Mitch Hankins <mitchhankins92@gmail.com>
2017-10-17 18:17:37 -0600

    Updated main app with state

--- src/index.js
+++ src/index.js
@@ -1,16 +1,29 @@
-import React from 'React';
+import React, { Component } from 'React';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/SearchBar';
 
 const API_KEY = 'AIzaSyCTmi-j4Tl8zD5KySWmzjrFsEGQ7FygLBY';
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] }
+
+    YTSearch({key: API_KEY, term: 'hello'}, videos => {
+      this.setState({ videos });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
+
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
