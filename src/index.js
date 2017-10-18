8d9bad4e8192b894cbedd9a99dc4bb50b589af21
Mitch Hankins <mitchhankins92@gmail.com>
2017-10-17 19:16:38 -0600

    Page now shows video, title, details, and video list dynamically

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/SearchBar';
 import VideoList from './components/VideoList';
+import VideoDetail from './components/VideoDetail';
 
 const API_KEY = 'AIzaSyCTmi-j4Tl8zD5KySWmzjrFsEGQ7FygLBY';
 
@@ -10,18 +11,23 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { videos: [] }
+    this.state = { videos: [], selectedVideo: null }
 
     YTSearch({key: API_KEY, term: 'hello'}, videos => {
-      this.setState({ videos });
+      this.setState({
+        videos,
+        selectedVideo: videos[0]
+      });
     });
   }
 
   render() {
-    const { videos } = this.state;
+    const { videos, selectedVideo } = this.state;
+
     return (
       <div>
         <SearchBar />
+        <VideoDetail video={selectedVideo} />
         <VideoList videos={videos} />
       </div>
     );
